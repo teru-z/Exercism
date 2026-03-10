4c98850a4d6525f7edd9d9bf185152550057181d
Teru <26590954+teru-z@users.noreply.github.com>
2026-03-10 20:48:19 +0900

    Go exercise 7 complete

---
+++ go/booking-up-for-beauty/booking_up_for_beauty.go
@@ -0,0 +1,38 @@
+package booking
+
+import (
+	"fmt"
+	"time"
+)
+
+// Schedule returns a time.Time from a string containing a date.
+func Schedule(date string) time.Time {
+	t, _ := time.Parse("1/2/2006 15:04:05", date)
+	return t
+}
+
+// HasPassed returns whether a date has passed.
+func HasPassed(date string) bool {
+	t, _ := time.Parse("January 2, 2006 15:04:05", date)
+	return time.Now().After(t)
+}
+
+// IsAfternoonAppointment returns whether a time is in the afternoon.
+func IsAfternoonAppointment(date string) bool {
+	t, _ := time.Parse("Monday, January 2, 2006 15:04:05", date)
+	h := t.Hour()
+	return h >= 12 && h < 18
+}
+
+// Description returns a formatted string of the appointment time.
+func Description(date string) string {
+	t, _ := time.Parse("1/2/2006 15:04:05", date)
+	formattedTimeString := t.Format("Monday, January 2, 2006, at 15:04")
+	return fmt.Sprintf("You have an appointment on %s.", formattedTimeString)
+}
+
+// AnniversaryDate returns a Time with this year's anniversary.
+func AnniversaryDate() time.Time {
+	y := time.Now().Year()
+	return time.Date(y, time.September, 15, 0, 0, 0, 0, time.UTC)
+}
